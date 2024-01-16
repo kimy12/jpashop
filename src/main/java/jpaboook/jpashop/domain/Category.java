25b3d99c78ee63a97723dac77ff3788730ee3992
kimy12 <shinkim7941@gmail.com>
2024-01-16 20:31:39 +0900

    연관 관계 편의 메서드 생성

--- src/main/java/jpaboook/jpashop/domain/Category.java
+++ src/main/java/jpaboook/jpashop/domain/Category.java
@@ -34,4 +34,12 @@ public class Category {
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    /**
+     * 연관관계 메서드 (양방향 세팅)
+     */
+    public void addChildCategory(Category child){
+        this.child.add(child);
+        child.setParent(this);
+    }
 }
--- src/main/java/jpaboook/jpashop/domain/Order.java
+++ src/main/java/jpaboook/jpashop/domain/Order.java
@@ -30,7 +30,7 @@ public class Order {
     /**
      * 연관관계의 주인으로 잡는다.
      */
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -38,4 +38,23 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 (ORDER , CAMCEL)
+
+    /**
+     * 연관관계 메서드 (양방향 세팅)
+     */
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+
+    }
 }
