fcfa3aab43d37532b5f4eae896a025e83626a82f
kimy12 <shinkim7941@gmail.com>
2024-01-21 19:37:42 +0900

    create js, css, and html

--- src/main/java/jpaboook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpaboook/jpashop/repository/OrderRepository.java
@@ -21,5 +21,15 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    //public List<Order> finalAll (OrderSearch orderSearch){}
+//    public List<Order> finalAll (OrderSearch orderSearch){
+//        em.createQuery("select o from Order o join o.member m" +
+//                " where o.status = :status " +
+//                " and m.name like :name" , Order.class)
+//                .setParameter("status", orderSearch.getOrderStatus())
+//                .setParameter("name", orderSearch.getMemberName())
+//                .setMaxResults(1000) // 최대 1000건
+//                .getResultList();
+//    }
+
+
 }
